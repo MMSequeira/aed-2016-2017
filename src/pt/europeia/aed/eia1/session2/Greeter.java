83686f663b65bcc6e7676569aec45ed0f41fa999
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2017-02-28 16:02:47 +0000

    Add session 2 files for EI-A1 and EI-A2.

---
+++ src/pt/europeia/aed/eia1/session2/Greeter.java
@@ -0,0 +1,11 @@
+package pt.europeia.aed.eia1.session2;
+
+import static java.lang.System.out;
+
+public class Greeter {
+
+    public static void main(final String[] arguments) {
+        out.println("Hello world! " + arguments.length + " " + arguments[0]);
+    }
+
+}
