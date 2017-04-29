af67a8d9b148853455e7024c87ea3111f81a3462
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2017-04-29 17:46:58 +0100

    Add selection sort tester.

---
+++ src/pt/europeia/aed/book/chapter2/section1/SelectionTester.java
@@ -0,0 +1,57 @@
+package pt.europeia.aed.book.chapter2.section1;
+
+import static pt.europeia.aed.Tools.out;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Arrays;
+
+public class SelectionTester {
+
+    public static void main(final String[] arguments) {
+        out.println("Testing insertion sort:");
+
+        StdRandom.setSeed(3467812342L);
+
+        final int numberOfRepetions = 10000;
+
+        for (int size = 1; size != 1 << 11; size <<= 1) {
+            out.println("\tTesting array size " + size + " with "
+                    + numberOfRepetions + " repetitions:");
+
+            for (int repetition = 0; repetition != numberOfRepetions; repetition++) {
+                final Double[] originalValues = randomDoubleArrayOf(size);
+                final Double[] systemSortedValues = originalValues.clone();
+                final Double[] sortedvalues = originalValues.clone();
+
+                Arrays.sort(systemSortedValues);
+
+                Selection.sort(sortedvalues);
+
+                if (!Arrays.equals(sortedvalues, systemSortedValues)) {
+                    out.println("\t\tError (repetition = " + repetition + "):");
+                    out.println("\t\t\tWas:       "
+                            + Arrays.toString(originalValues));
+                    out.println("\t\t\tShould be: "
+                            + Arrays.toString(systemSortedValues));
+                    out.println("\t\t\tIs:        "
+                            + Arrays.toString(sortedvalues));
+                }
+            }
+
+            out.println("\tFinished tests with array size " + size + ".");
+        }
+
+        out.println("Finished all tests.");
+    }
+
+    private static Double[] randomDoubleArrayOf(final int size) {
+        final Double[] values = new Double[size];
+
+        for (int i = 0; i != values.length; i++)
+            values[i] = Math.round(StdRandom.uniform() * 100.0) / 100.0;
+
+        return values;
+    }
+
+}
